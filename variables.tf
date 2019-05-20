e820a9880bd86804034202e0d1c25b42f9c4092c
Jacob <jacobm3@gmail.com>
2019-05-20 14:01:18 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "bucket_name" {
     type = "string"
     description = "Name of the bucket. Must be between 3 and 63 characters long, and can contain only lower-case characters, numbers, periods, and dashes."
 }
+
+variable "tfe_token" {
+    type = "string"
+}

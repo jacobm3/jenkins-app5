c0531339b48f598754fe78778c04a12ca9ac30d4
Jacob <jacobm3@gmail.com>
2019-05-20 16:55:10 -0500

    Update tfe.tf

--- tfe.tf
+++ tfe.tf
@@ -2,8 +2,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "jacobm3"
-    token = "${var.tfe_token}"
-
+    
     workspaces {
       name = "jenkins-app5-dev"
     }

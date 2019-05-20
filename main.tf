ec4929559131f01bfeb60364a73f191199682221
Jacob <jacobm3@gmail.com>
2019-05-20 12:57:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-resource "random_string" "random" {
-  length = 16
-  special = false
-}
-
 module "dev_bucket" {
   source  = "app.terraform.io/jacobm3/s3_simple/aws"
-  version = "1.0"
   bucket_name = "marlene-dev-bucket-${var.random}"
 }

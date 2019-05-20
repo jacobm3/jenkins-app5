fa937413030b356e230956855e3cec7c9f07eee4
Jacob <jacobm3@gmail.com>
2019-05-20 13:36:27 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "bucket_endpoint" {
     value = "${module.dev_bucket.website_bucket_id}"
 }
+
+output "bucket_domain_name" {
+    value = "${module.dev_bucket.domain_name}"
+}
+
+output "bucket_arn" {
+    value = "${module.dev_bucket.arn}"
+}

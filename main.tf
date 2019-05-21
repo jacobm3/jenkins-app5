b5c7368492a359765c738e9642392b641b2eef47
Jacob <jacobm3@gmail.com>
2019-05-21 09:34:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,41 @@ module "dev_bucket" {
   
 }
 
+resource "aws_instance" "vm" {
+  ami          = "ami-2757f631"
+  instance_type = "t2.micro"
+  count        = 1
+}
+  
+resource "aws_s3_bucket" "website_bucket" {
+  force_destroy = true
+  bucket = "jmartinson-23497230948230480923843243"
+  acl    = "public-read"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "HEAD"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+  versioning {
+    enabled = true
+  }
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.bucket_name}/*"
+        }
+    ]
+}
+  POLICY
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}

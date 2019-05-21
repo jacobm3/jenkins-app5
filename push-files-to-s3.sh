d52ab8dcd977fd13e520f3a964c2adbb4940b145
Jacob <jacobm3@gmail.com>
2019-05-21 01:19:17 -0500

    Update push-files-to-s3.sh

--- push-files-to-s3.sh
+++ push-files-to-s3.sh
@@ -1,4 +1,3 @@
-cd files 
 AWS_ACCESS_KEY_ID=`vault kv get -field=AWS_ACCESS_KEY_ID kv/jenkins-creds` \
 AWS_SECRET_ACCESS_KEY=`vault kv get -field=AWS_SECRET_ACCESS_KEY kv/jenkins-creds` \
-aws s3 cp --recursive * s3://`terraform output -json | /usr/bin/jq -r .bucket_endpoint.value`
+aws s3 cp --recursive files/* s3://`terraform output -json | /usr/bin/jq -r .bucket_endpoint.value`

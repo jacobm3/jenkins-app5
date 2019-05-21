e617efd5a29af107fb70bb61f12348e5214ed551
Jacob <jacobm3@gmail.com>
2019-05-21 00:57:47 -0500

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,3 @@
+AWS_ACCESS_KEY_ID=`vault kv get -field=AWS_ACCESS_KEY_ID kv/jenkins-creds` \
+AWS_SECRET_ACCESS_KEY=`vault kv get -field=AWS_SECRET_ACCESS_KEY kv/jenkins-creds` \
+aws s3 cp files/index.html s3://`terraform output -json | /usr/bin/jq -r .bucket_endpoint.value`

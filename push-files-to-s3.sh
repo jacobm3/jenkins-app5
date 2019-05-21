551941dae0937a01eb8d5e4dc7b269c997760392
Jacob <jacobm3@gmail.com>
2019-05-21 01:26:47 -0500

    Delete push-files-to-s3.sh

--- push-files-to-s3.sh
+++
@@ -1,3 +0,0 @@
-AWS_ACCESS_KEY_ID=`vault kv get -field=AWS_ACCESS_KEY_ID kv/jenkins-creds` \
-AWS_SECRET_ACCESS_KEY=`vault kv get -field=AWS_SECRET_ACCESS_KEY kv/jenkins-creds` \
-aws s3 cp files/* s3://`terraform output -json | /usr/bin/jq -r .bucket_endpoint.value`
